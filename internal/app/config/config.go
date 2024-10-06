1835983d16e6b75528366a86677752140c3ffc74
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2024-10-06 10:45:31 +0300

    feat: env added

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,7 +2,14 @@ package config
 
 import (
 	"flag"
+	"github.com/caarlos0/env/v6"
+	"log"
 )
+
+type envConfig struct {
+	FlagRunAddr     string `env:"SERVER_ADDRESS"`
+	BaseShorterAddr string `env:"BASE_URL"`
+}
 
 var (
 	FlagRunAddr     string
@@ -13,9 +20,25 @@ func ParseFlags() {
 	flag.StringVar(&FlagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&BaseShorterAddr, "b", "http://localhost:8080", "base url of shorter address")
 	flag.Parse()
+	parsEnv()
 }
 
 func SetConfig(runAddr, baseAddr string) {
 	FlagRunAddr = runAddr
 	BaseShorterAddr = baseAddr
+	parsEnv()
+}
+
+func parsEnv() {
+	var cfg envConfig
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(cfg.BaseShorterAddr) > 0 {
+		BaseShorterAddr = cfg.BaseShorterAddr
+	}
+	if len(cfg.FlagRunAddr) > 0 {
+		BaseShorterAddr = cfg.FlagRunAddr
+	}
 }
